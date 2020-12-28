daa5eec25703122856e7f435e1407e79e789f83d
AYUROOTS <ayuroots.office@gmail.com>
2020-12-28 17:08:44 +0530

    coming soon

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("volume-btn");
+const icon = document.getElementById("volume-btn").firstChild;
+const audio = document.getElementById("player");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-mute');
+
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-mute');
+    icon.classList.add('fa-volume-up');
+  }
+});
